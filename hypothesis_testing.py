a43f5e23134969a7dd5141c96b0d0863be5ce8fc
David Wederstrandt Sr <david.wederstrandt@gmail.com>
2020-03-24 11:13:15 -0500

    adding hypothisis_testing.py

---
+++ hypothesis_testing.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## For each of the following questions, formulate a null and alternative hypothesis (be as specific as you can be), then give an example of what a true positive, true negative, type I and type II errors would look like. Note that some of the questions are intentionally phrased in a vague way. It is your job to reword these as more precise questions that could be tested.
+
+# Has the network latency gone up since we switched internet service providers?
+
+# - Ho: Switching internet providers has no effect on network latency
+# - Ha: Switching internet providers has increased network latency
+
+# Is the website redesign any good?
+
+# - Ho: The redesign has not improved the website
+# + Ha: The redesign has imporved the website
+
+# Is our television ad driving more sales?
+
+# - Ho: The television ads have had no effect on sales
+# - Ha: The television ads have increased sales
